5a3b5eda9d74fdb9fb8368810f57830ed7628f35
Pacien Boisson <pakokrew@gmail.com>
2015-08-03 15:48:30 +0200

    Debut scene

--- app/js/scene.js
+++ app/js/scene.js
@@ -11,7 +11,8 @@ function MainScene() {
         self.scene = new THREE.Scene();
 
         self.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        self.camera.position.z = 1000;
+        self.camera.position.y = 100;
+        self.camera.position.z = 500;
 
         var light = new THREE.AmbientLight( 0x404040 ); // soft white light
         self.scene.add( light );
@@ -19,23 +20,43 @@ function MainScene() {
         var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
         directionalLight.position.set( 0, 1, 0 );
         self.scene.add( directionalLight );
-    }
+
+      	var axes = new THREE.AxisHelper(100);
+      	self.scene.add( axes );
+
+        var floorMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, side: THREE.DoubleSide } );
+      	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
+      	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+      	floor.position.y = -0.5;
+      	floor.rotation.x = Math.PI / 2;
+      	self.scene.add(floor);
+
+
+      	// make sure the camera's "far" value is large enough so that it will render the skyBox!
+      	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+      	// BackSide: render faces from inside of the cube, instead of from outside (default).
+      	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+      	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+      	self.scene.add(skyBox);
+    };
 
     self.populate = function() {
 
         _tyranosaur = new Tyranosaur();
+        _tyranosaur.getObject().position.y = 30;
 
         self.scene.add(_tyranosaur.getObject());
 
         _sceneReady = true;
-    }
+
+    };
 
     self.step = function() {
 
         if(!_sceneReady) return;
 
-        _tyranosaur.rotate();
-    }
+        _tyranosaur.idleAnimation();
+    };
 
 
     constructor();
--- app/js/tyranosaur.js
+++ app/js/tyranosaur.js
@@ -4,6 +4,8 @@ function Tyranosaur() {
     var geometry, material, mesh;
     var object;
 
+    var rotationStep = 0.02;
+
     function constructor() {
 
         var model = _.find(models, {name: 'dino'});
@@ -13,7 +15,9 @@ function Tyranosaur() {
             return;
         }
 
-        object = model.object;
+        model.object.rotation.y = -Math.PI/2;
+        object = new THREE.Object3D();
+        object.add(model.object);
 
         geometry = new THREE.BoxGeometry( 200, 200, 200 );
         material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
@@ -28,6 +32,15 @@ function Tyranosaur() {
     self.rotate = function() {
         object.rotation.x += 0.01;
         object.rotation.y += 0.02;
+    };
+
+    self.idleAnimation = function() {
+      var angle_max = Math.PI/5;
+      if(object.rotation.y + rotationStep > angle_max || object.rotation.y + rotationStep < -angle_max) {
+        rotationStep *= -1;
+      }
+      object.rotation.y += rotationStep;
+
     };
 
     constructor();
