2fc6d269519beebad7cd26a00d162b01e9d7532f
Pacien Boisson <pakokrew@gmail.com>
2015-09-23 12:10:57 +0200

    Tests config

--- tests/karmaClient.conf.js
+++ tests/karmaClient.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dot', 'coverage'],
+        reporters: ['dots', 'coverage'],
 
         coverageReporter: {
             dir : 'coverage/client/',
